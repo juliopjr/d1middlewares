b75efa471714f1551d6def62eadcf9ead54d9fd3
Julio P. Jr <juliopjr.dev@gmail.com>
2023-04-03 16:25:13 -0300

    check if user exists by username

--- src/index.js
+++ src/index.js
@@ -10,7 +10,16 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const userExists = users.find(user => user.username === username);
+  if(!userExists) {
+    return response.status(404).send();
+  }
+
+  request.user = userExists;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
